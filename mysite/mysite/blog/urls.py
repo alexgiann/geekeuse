ad332c3f87c68339d59306a97129680b7b5e448e
alexandragiannopoulou <alex.giannopoulou@gmail.com>
2015-04-09 22:48:16 +0200

    urls change

--- mysite/mysite/blog/urls.py
+++ mysite/mysite/blog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import include, url
 from . import views
 
@@ -5,3 +6,5 @@ urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
